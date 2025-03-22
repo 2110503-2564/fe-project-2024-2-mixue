84a0ac3c1727a060d8334a8c02349e8589034ab4
FreezingH2O <airadajitauleeruk@gmail.com>
2025-03-22 22:40:57 +0700

    edit appointment list

--- src/app/(dentist_info)/dentist/addDentist/page.tsx
+++ src/app/(dentist_info)/dentist/addDentist/page.tsx
@@ -1,12 +1,8 @@
-
 import DentistForm from "@/components/DentistForm";
 export default function addDentistPage() {
-  
-
   return (
     <div>
        <DentistForm ></DentistForm>
-
     </div>
     
   );
